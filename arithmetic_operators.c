700af13d6e9e3c8a45f95e001650e763562e0910
Shivam <shivanmishra734@gmail.com>
2026-06-24 23:52:35 +0530

    Learning Arithmetic Operators

--- arithmetic_operators.c
+++ arithmetic_operators.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
-int main() {
+int main()
+{
     int a = 10;
     int b = 5;
-    int sum = a + b; // Addition
-    int difference = a - b; // Subtraction
-    int product = a * b; // Multiplication
+    int sum = a + b;               // Addition
+    int difference = a - b;        // Subtraction
+    int product = a * b;           // Multiplication
     float quotient = (float)a / b; // Division
-    int remainder = a % b; // (%) is the sign to calculate remainder,also known as modulus operator
+    int remainder = a % b;         // (%) is the sign to calculate remainder,also known as modulus operator
+    /* % is the modular division operator
+o
+% → returns the remainder
+o
+% → cannot be applied on float
+o
+% → sign is same as of numerator (-5%2=-1) */
 
     printf("Sum: %d\n", sum);
     printf("Difference: %d\n", difference);
     printf("Product: %d\n", product);
     printf("Quotient: %.2f\n", quotient); // %.2f formats the float to 2 decimal places
     printf("Remainder: %d\n", remainder);
-/*int b = 2, c = 3; 
-int z; z = b*c; //legal 
-int z; b*c = z; //illegal (not allowed)*/
+    /*int b = 2, c = 3;
+    int z; z = b*c; //legal
+    int z; b*c = z; //illegal (not allowed)*/
     return 0;
 }
